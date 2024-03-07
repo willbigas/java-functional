86f2ceb84f8eb46e797c79a4d09142ab99a83261
William Bigas Mauro <williambmauro@hotmail.com>
2024-03-07 11:59:40 -0300

    Adicionado BiFunction

--- src/main/java/br/com/willbigas/functionalinterface/_Function.java
+++ src/main/java/br/com/willbigas/functionalinterface/_Function.java
@@ -1,11 +1,13 @@
 package br.com.willbigas.functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
 
 	public static void main(String[] args) {
 
+		// Functional with 1 argument and produce 1 result
 		System.out.println("Increment Method");
 		int increment = incrementByOne(1);
 		System.out.println(increment);
@@ -26,6 +28,19 @@ public class _Function {
 		Integer apply = addBy1AndThenMultiplyBy10.apply(10);
 		System.out.println(apply);
 
+
+		//BiFunction with 2 arguments and produce 1 result
+
+		System.out.println("Increment and Multiply - Method normal");
+		int incrementAndMultiply = incrementByOneAndMultiply(4, 100);
+		System.out.println(incrementAndMultiply);
+
+
+		System.out.println("Increment and Multiply - BiFunction");
+		int incrementAndMultiplyFunction = incrementByOneAndMultiplyFunction.apply(4, 100);
+		System.out.println(incrementAndMultiplyFunction);
+
+
 	}
 
 
@@ -37,5 +52,22 @@ public class _Function {
 
 	static int incrementByOne(int number) {
 		return number + 1;
+	}
+
+
+	/**
+	 * Funcao com 2 parâmetros que retona um valor
+	 */
+	static BiFunction<Integer,  Integer , Integer> incrementByOneAndMultiplyFunction =
+			(numberToIncrementByOne , numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+	/**
+	 * Metodo convencional do Bifunction
+	 * @param number
+	 * @param numToMultiplyBy
+	 * @return
+	 */
+	static int incrementByOneAndMultiply(int number , int numToMultiplyBy) {
+		return (number + 1) * numToMultiplyBy;
 	}
 }
